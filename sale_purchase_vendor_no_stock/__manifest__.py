e4c1395462f795f091442a77ba354e9b1b66171a
Deriman-Alonso <161326520+Deriman-Alonso@users.noreply.github.com>
2024-04-16 08:35:49 +0100

    Initial commit

---
+++ sale_purchase_vendor_no_stock/__manifest__.py
@@ -0,0 +1,18 @@
+#  GNU nano 6.2
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Purchase Vendor No Stock",
+    "version": "17.0.1.0.0",
+    "category": "Category",
+    "author": "<Odoo Community Association (OCA)>",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+
+    ],
+}
