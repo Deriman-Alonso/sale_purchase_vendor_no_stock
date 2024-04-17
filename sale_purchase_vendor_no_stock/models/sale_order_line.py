9ffeafa1db720f7e051b0fac132e6113993c48a7
David-Luis-Mora <d.luis@binhex.cloud>
2024-04-17 14:54:46 +0100

    [ADD] Views and models

--- sale_purchase_vendor_no_stock/models/sale_order_line.py
+++ sale_purchase_vendor_no_stock/models/sale_order_line.py
@@ -5,20 +5,23 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     vendor_id = fields.Many2one(
-        comodel_name="res.partner",
-        string="Vendor",
+        "res.partner", string="Vendor", search="_search_vendor_id"
     )
     vendor_id_domain = fields.Binary(
-        compute="_compute_vendor_id_domain",
-        readonly=True,
-        store=False,
+        compute="_compute_vendor_id_domain", store=False, readonly=True
     )
 
     @api.depends("product_id")
     def _compute_vendor_id_domain(self):
         for item in self:
-            domain = [("id", "in", item.product_id.variant_seller_ids.partner_id.ids)]
-            item.vendor_id_domain = domain
+            # domain = (
+            #     [("id", "in", item.product_id.variant_seller_ids.ids)]
+            #     if item.order_id
+            #     else []
+            # )
+            # item.vendor_id_domain = json.dumps(domain)
+            # import wdb;wdb.set_trace()
+            item.vendor_id_domain = item.product_id.variant_seller_ids.ids
 
     def _prepare_procurement_values(self, group_id=False):
         """Inject in the procurement values the preferred vendor if any, and create
@@ -33,19 +36,13 @@ class SaleOrderLine(models.Model):
                 uom_id=self.product_uom,
             )
             if not suppinfo:
-                # By default user with group_sale_salesman group can not creates
-                # supplierinfo records.
-                suppinfo = (
-                    self.env["product.supplierinfo"]
-                    .sudo()
-                    .create(
-                        {
-                            "product_tmpl_id": product.product_tmpl_id.id,
-                            "partner_id": self.vendor_id.id,
-                            "min_qty": 0,
-                            "company_id": self.company_id.id,
-                        }
-                    )
+                suppinfo = self.env["product.supplierinfo"].create(
+                    {
+                        "product_tmpl_id": product.product_tmpl_id.id,
+                        "name": self.vendor_id.id,
+                        "min_qty": 0,
+                        "company_id": self.company_id.id,
+                    }
                 )
             res["supplierinfo_id"] = suppinfo
         return res
